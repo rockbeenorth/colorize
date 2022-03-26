88eaeb0328054a7c56837b43e107b736b3546948
Ivan Vasilev <rockbee@rockbee.com>
2022-03-26 12:03:30 +0000

    feat: render jsonified collections

--- rb_colorize/__init__.py
+++ rb_colorize/__init__.py
@@ -37,15 +37,6 @@ def main(h:int, s=100) -> list:
 
 if __name__ == "__main__":
     
-    # import json
-
-    # jsonified = json.dumps(swatcher(341), sort_keys=True, indent=4)
-
-    # print(jsonified)
-
-    # with open("../output/colors.json", "w") as f:
-    #     f.write(jsonified)
-
     x = main(341)
     print(x)
 
@@ -55,62 +46,3 @@ if __name__ == "__main__":
     render_css_colors(341)
     index_html()
 
-
-
-
-# def swatcher(
-#     hue: int,
-#     steps: int,
-#     saturation = 100,
-#     lightness = 100,
-#     opacity = 1,
-#     name = "main",
-#     detailed = True,
-#     dark_desaturate = 5,
-#     ) -> dict:
-
-#     params = {
-#         "hue" : hue,
-#         "steps" : steps,
-#         "saturation" : saturation,
-#         "lightness" : lightness,
-#         "opacity" : opacity,
-#         "detailed" : detailed,
-#         "dark_desaturate" : dark_desaturate,
-#     }
-
-
-#     if steps > 100:
-#         steps = 100
-    
-#     if steps < 0:
-#         steps = 1
-    
-#     if saturation > 100:
-#         saturation = 100
-    
-#     if saturation < 0:
-#         saturation = 0
-    
-#     if lightness > 100:
-#         lightness = 100
-    
-#     if lightness < 0:
-#         lightness = 0
-
-#     hue = degree_correction(hue)
-#     # second = degree_correction(hue-120)
-#     # third = degree_correction(hue+120)
-
-    
-#     palettes = {}
-    
-#     for palette in names:
-#         palettes[palette] = generate_swatches_list(**params, name=palette)
-
-#     # GENERATE GRAYSCALE IF THE ORIGINAL WAS NOT GRAY
-#     if saturation != 0:
-#         palettes["grayscale"] = generate_swatches_list(0, steps, 0, lightness, opacity, "grayscale", True)
-
-    
-#     return palettes    
