fbcbe2b7029548bd3b3a5a6dcb706611dd9a0854
Ivan Vasilev <rockbee@rockbee.com>
2022-05-28 22:17:47 +0100

    wip: get color function in pythin and js

--- getColor.js
+++ getColor.js
@@ -1,4 +1,4 @@
-function getColor(h, level, s=90) {
+function getColor(h, level, s = 90) {
 
     // Lightness scales for light and dark modes
     const SCALE_LIGHT = [98, 96, 94, 90, 86, 81, 74, 66, 55, 39]
@@ -10,17 +10,17 @@ function getColor(h, level, s=90) {
     // TODO: check if saturation is less then DARK_DESATURATE and if so, don't desaturate?
 
     // Check if level is within range 1-10
-    if (level > 10) {level = 10}
-    else if (level < 1) {level = 2}
+    if (level > 10) { level = 10 }
+    else if (level < 1) { level = 2 }
 
     // TODO: return absolute color -- hsl(hue, 100%, 50%) -- if level > 10
 
     // Check if hue is within range 0-360:
-    if (h < 0 || h > 360) {h = h % 360}
+    if (h < 0 || h > 360) { h = h % 360 }
 
     // prepare HSL colors:
-    const light = `hsl: (${h}, ${s}%, ${SCALE_LIGHT[level-1]}%)`
-    const dark = `hsl: (${h}, ${s-DARK_DESATURATE}%, ${SCALE_DARK[level-1]}%)`
+    const light = `hsl: (${h}, ${s}%, ${SCALE_LIGHT[level - 1]}%)`
+    const dark = `hsl: (${h}, ${s - DARK_DESATURATE}%, ${SCALE_DARK[level - 1]}%)`
 
     return [light, dark]
 
