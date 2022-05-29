6f2547259028b0faa3571f0b7f81e120b6db9360
Ivan Vasilev <rockbee@rockbee.com>
2022-05-29 12:11:30 +0100

    function colorLevel() in js

--- getColor.js
+++ getColor.js
@@ -5,28 +5,28 @@ function colorLevel(h, level = 0, s = 90) {
     }
 
     // Lightness scales for light and dark modes
-    const SCALE_LIGHT = [98, 96, 94, 90, 86, 81, 74, 66, 55, 39]
-    const SCALE_DARK = [4, 6, 8, 12, 16, 21, 28, 36, 47, 63]
+    const SCALE_LIGHT = [98, 96, 94, 90, 86, 81, 74, 66, 55, 39];
+    const SCALE_DARK = [4, 6, 8, 12, 16, 21, 28, 36, 47, 63];
 
     // Define step to desaturate color for dark mode
-    let darkDesaturate = 10
+    const DARK_DESATURATE = 10;
 
-    // If saturation is less then darkDesaturate don't desaturate:
-
-    if (s <= darkDesaturate) {
-        darkDesaturate = 0;
-    }
+    // Don't desaturate if saturation is less than DARK_DESATURATE:
+    let darkDesaturate;
+    if (s <= DARK_DESATURATE) {
+         darkDesaturate = 0;
+    } else { darkDesaturate = DARK_DESATURATE; }
 
     // Normalize the hue to the range 0-360.
     // It is NOT an error, because sometimes you need to calculate 
     // the opposite or additional hue number and it can be out of range 
     // but still remain a valid color.
 
-    if (h > 360) { h = h % 360 }
+    if (h > 360) { h = h % 360 };
     if (h < 0) {
         let delta = Math.abs(h) % 360;
         h = 360 - delta;
-    }
+    };
 
     // Return the absolute/default color for level 0 -- hsl(hue, 100%, 50%)
     // Level 0 returns absulute color with maxed out saturation and 50%
@@ -37,24 +37,24 @@ function colorLevel(h, level = 0, s = 90) {
             `hsl(${h}, 100%, 50%)`,
             `hsl(${h}, ${100 - darkDesaturate}%, 50%)`
         ]
-    }
+    };
 
     // Check if level is within range 1-10
 
     if (level < 0 || level > 10) {
         throw `The level argument should be between 0 and 10, but ${level} was given instead.`
-    }
+    };
 
     // prepare HSL colors:
-    const light = `hsl(${h}, ${s}%, ${SCALE_LIGHT[level - 1]}%)`
-    const dark = `hsl(${h}, ${s - darkDesaturate}%, ${SCALE_DARK[level - 1]}%)`
+    const light = `hsl(${h}, ${s}%, ${SCALE_LIGHT[level - 1]}%)`;
+    const dark = `hsl(${h}, ${s - darkDesaturate}%, ${SCALE_DARK[level - 1]}%)`;
 
     return [light, dark]
 
 }
 
-// console.log(colorLevel(360, 5))
-// > [ 'hsl: (1, 90%, 39%)', 'hsl: (1, 80%, 63%)' ]
+console.log(colorLevel(4,3,0)) // returns gray color
 
-
-console.log(colorLevel(-17, 7))
+// for (let i = 0; i <= 360; i++) {
+//     console.log(colorLevel(i,7))
+// }
